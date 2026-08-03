10754d348477ec0e9e4884f7b786646bbb58619a
edward <edwardlfish@gmail.com>
2026-08-03 19:03:19 +0300

    Implement findBookAuthors service with book validation and author associations
    - Fetch book by ISBN and validate existence before querying authors
    - Query authors associated with book using getAuthors method
    - Exclude timestamps and junction table attributes from response
    - Format birth_date as birthDate in author attributes

--- src/services/author.service.js
+++ src/services/author.service.js
@@ -1,5 +1,18 @@
+import * as bookRepository from "../repositories/book.repository.js";
+import {sequelize} from "../config/database.js";
+
 export const findBookAuthors = async (isbn)  => {
-    // TODO: Implement findBookAuthors service
+    const book = await bookRepository.findBookById(isbn);
+    if (!book) {
+        throw new Error(`Book with ISBN ${isbn} not found`);
+    }
+    return await book.getAuthors({
+        attributes: {
+            exclude: ['createdAt', 'updatedAt', 'birth_date'],
+            include: ['name', [sequelize.col('birth_date'), 'birthDate']]
+        },
+        joinTableAttributes: []
+    });
 }
 
 export const removeAuthor = async (authorName) => {
